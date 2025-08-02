ffed5b78efeacf61a549f1254b85fa7af52f03f6
tirthpatel-7 <tirth2240@gmail.com>
2025-08-02 16:46:13 +0530

    api folder

--- pintrest/routes/users.js
+++ pintrest/routes/users.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 const plm = require('passport-local-mongoose')
-
-mongoose.connect('mongodb://127.0.0.1:27017/pintrestproject')
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost:27017/pintrest", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const user = mongoose.Schema({
   username: {
