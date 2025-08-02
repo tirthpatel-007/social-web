1c48ff81bac58e9e0814ffb9c3494d3c879db7d0
tirthpatel-7 <tirth2240@gmail.com>
2025-08-02 17:05:55 +0530

    app.js

--- pintrest/app.js
+++ pintrest/app.js
@@ -61,8 +61,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-app.listen(port, host, () => {
-  console.log(`your server is running on ${host}:${port}`)
-})
+// app.listen(port, host, () => {
+//   console.log(`your server is running on ${host}:${port}`)
+// })
 
 module.exports = app;
