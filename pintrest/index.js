6e515411c89a92b6f6f0578869d519b62687ea32
tirthpatel-7 <tirth2240@gmail.com>
2025-08-03 15:36:20 +0530

    index.js

--- pintrest/index.js
+++ pintrest/index.js
@@ -1,5 +1,3 @@
-// A simple test function to see if Vercel routing works.
-module.exports = (req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
-  res.status(200).send('Hello from Vercel! If you can see this, the routing is fixed.');
-};
+// PUT THIS CODE BACK
+const app = require('./app');
+module.exports = app;
