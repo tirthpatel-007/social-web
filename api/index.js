585f5967e666b0851507cea8d841430f049c23fa
tirthpatel-7 <tirth2240@gmail.com>
2025-08-06 23:27:03 +0530

    index.js vercel.json

--- api/index.js
+++ api/index.js
@@ -1,14 +1,12 @@
 // Load environment variables
 require('dotenv').config();
 
-// Import the configured Express app from your src folder
-const app = require('../src/app.js');
-
-const app = require('../index');
-
-// Import and run the database connection
-const connectDB = require('../src/config/db.js');
+// Import and run the database connection first
+const connectDB = require('../config/db.js');
 connectDB();
+
+// Import the configured Express app
+const app = require('../app.js');
 
 // Export the app for Vercel's serverless environment
 module.exports = app;
