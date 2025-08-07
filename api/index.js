3f3c53d86b0721206cc4d5b9b55f08c0818506ff
tirthpatel-7 <tirth2240@gmail.com>
2025-08-07 10:33:03 +0530

    api/index.js

--- api/index.js
+++ api/index.js
@@ -2,11 +2,11 @@
 require('dotenv').config();
 
 // Import and run the database connection first
-const connectDB = require('../config/db.js');
+const connectDB = require('../src/config/db.js');
 connectDB();
 
 // Import the configured Express app
-const app = require('../app.js');
+const app = require('../src/app.js');
 
 // Export the app for Vercel's serverless environment
 module.exports = app;
--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -2,7 +2,6 @@ const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
-// Configure Cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
